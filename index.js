2b39d7eacc98d3d9ef37e95dc90a5a0f4b210bb8
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-08-29 16:47:54 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let Display=document.querySelector('.display');
+buttons=document.querySelectorAll('button');
+let screenvalue='';
+        for(item of buttons){
+            item.addEventListener('click',(e)=>{
+                buttontext=e.target.innerText;
+                console.log('muji', buttontext);
+                if(buttontext=='C')
+                {
+                    screenvalue='0';
+                    Display.value=screenvalue; 
+                }
+                else if(buttontext=='='){
+                    Display.value=eval(screenvalue);
+                }
+                else{
+                    screenvalue +=buttontext;
+                    Display.value=screenvalue;
+                }
+            })
+        }
